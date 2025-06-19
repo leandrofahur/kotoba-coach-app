48c4e14b0489d8c50e675263120f254ac5a9ca68
Leandro Miranda Fahur Machado <leandrofahur@gmail.com>
2025-06-19 13:36:12 -0700

    hardcode strcture for now

--- backend/app/api/v1/routes/phrase.py
+++ backend/app/api/v1/routes/phrase.py
@@ -1,13 +1,15 @@
+from typing import List
 from fastapi import APIRouter
+
+from models.phrase import Phrase
 from services.phrase_service import get_all_phrases, get_phrase_by_id
 
 router = APIRouter(prefix="/phrase", tags=["Phrase"])
 
 @router.get("/")
-def get_phrases(id: str = None):
-    if not id:
-        return get_all_phrases()
-    else:
-        return get_phrase_by_id(id)
-
-
+def get_phrases() -> List[Phrase]:
+    return get_all_phrases()
+    
+@router.get("/{id}")
+def get_phrase(id: str) -> Phrase:
+    return get_phrase_by_id(id)
