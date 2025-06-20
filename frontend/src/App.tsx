bd368e18d6c695a601473565f1596a119d2bbc8d
Leandro Miranda Fahur Machado <leandrofahur@gmail.com>
2025-06-20 10:29:33 -0700

    tailwind 4 with shadcn working like a charm

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@/components/ui/button"
+
 function App() {
   return (    
     <div className="flex flex-col items-center justify-center h-screen bg-beige-light">
       <h1 className="text-3xl font-bold underline text-primary-dark">Hello World</h1>
       <p className="text-gray-dark">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
+      <Button>Click me</Button>
     </div>
   )
 }
