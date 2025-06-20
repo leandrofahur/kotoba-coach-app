6b2ff7a9027e128a31fd295aef6d07f84f87eb44
Leandro Miranda Fahur Machado <leandrofahur@gmail.com>
2025-06-20 10:11:24 -0700

    React + TS + Vite + latest Talwind

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
     port: 5173,
     host: '0.0.0.0',
     strictPort: true,
   },  
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@/components/*": path.resolve(__dirname, "./src/components/*"),
+      "@/lib/*": path.resolve(__dirname, "./src/lib/*"),
+    },
+  },
 })
